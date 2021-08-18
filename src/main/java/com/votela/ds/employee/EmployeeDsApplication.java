f2d6b7a426a4bd4592ad3920460894197c8ea791
Mkhululi47 <mkhululi.mtshemla1@gmail.com>
2021-08-19 01:48:17 +0200

    add command runner

--- src/main/java/com/votela/ds/employee/EmployeeDsApplication.java
+++ src/main/java/com/votela/ds/employee/EmployeeDsApplication.java
@@ -2,12 +2,16 @@ package com.votela.ds.employee;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EmployeeDsApplication {
@@ -25,6 +29,21 @@ public class EmployeeDsApplication {
 			Integer port = serverProperties.getPort();
 
 			LOG.info("Embedded Keycloak started: http://localhost:{} to use keycloak", port);
+		};
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
 		};
 	}
 }
